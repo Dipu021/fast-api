eead207f1cf9e11618be841fd30b8ea903661c38
Dipu Thakur <diput7012@gmail.com>
2026-09-14 22:49:45 +0545

    SQLite database integration example

---
+++ database_integration.py
@@ -0,0 +1,23 @@
+import sqlite3
+from fastapi import FastAPI
+
+app = FastAPI()
+
+conn = sqlite3.connect("Test.db",check_same_thread=False)
+
+cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS todos(
+        id INTEGER PRIMARY KEY,
+        title TEXT,
+        complete TEXT)
+""")
+
+conn.commit()
+
+@app.get("/")
+def home():
+    return{
+        "Message":"SQLite Connected Successfully"
+    }
