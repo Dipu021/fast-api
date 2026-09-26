e4953ebf71f1975b3a88f2d4cab1d39edaa90320
Dipu Thakur <diput7012@gmail.com>
2026-09-26 15:11:12 +0545

    Added the source of the webscrawling using beautufulsoup and fastapi

---
+++ web_crawling.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+import requests
+from bs4 import BeautifulSoup
+
+app = FastAPI()
+
+@app.get("/news")
+def get_news():
+    url = "https://indianexpress.com/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text,"html.parser")
+
+    title = []
+
+    for item in soup.find_all("a",class_="article-click topblockNews__sidebarLink"):
+        title.append(item.text)
+
+    return{
+        "News":title
+    }
