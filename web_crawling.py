e02bbecd2635552a381c98b35ce1e94a87588ba2
Dipu Thakur <diput7012@gmail.com>
2026-09-26 15:13:19 +0545

    Update the code

--- web_crawling.py
+++ web_crawling.py
@@ -6,13 +6,13 @@ app = FastAPI()
 
 @app.get("/news")
 def get_news():
-    url = "https://indianexpress.com/"
+    url = "--------"  #you can put the url of website , remember not every website allow scraping
     response = requests.get(url)
     soup = BeautifulSoup(response.text,"html.parser")
 
     title = []
 
-    for item in soup.find_all("a",class_="article-click topblockNews__sidebarLink"):
+    for item in soup.find_all("--",class_="---------"): # the blank is the xpath of specific field like header or anything
         title.append(item.text)
 
     return{
